44b9317fc4fa8a86b53979af047ae6daa61a321f
Bruno Romeiro <bruno.romeiro@gmail.com>
2020-08-14 16:27:29 -0300

    refactor: 💡 Update Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MyCounterComponent} from './my-counter/my-counter.component';
+import {MoviesPageComponent} from './movies-page/movies-page.component';
 
 const routes: Routes = [
-  {path: 'counter', component: MyCounterComponent}
+  {path: 'counter', component: MyCounterComponent},
+  {path: 'movies', component: MoviesPageComponent},
 ];
 
 @NgModule({
