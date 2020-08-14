913658e0f6810e512aee8d4060f1f046e49efbb7
Bruno Romeiro <bruno.romeiro@gmail.com>
2020-08-14 16:18:56 -0300

    feat: 🎸 Implements Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MyCounterComponent} from './my-counter/my-counter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'counter', component: MyCounterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
